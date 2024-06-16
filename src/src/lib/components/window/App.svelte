8a4e18aa838dd01d051a3fef1e89882912681550
Picorims <picorims.contact@gmail.com>
2024-06-16 17:10:15 +0200

    🌈 style(splitter): add splitter icon

---
+++ src/src/lib/components/window/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { Pane, Splitpanes } from 'svelte-splitpanes';
+	import { GripHorizontal, GripVertical } from 'lucide-svelte';
+</script>
+
+<Splitpanes class="main-split-pane" theme="custom-theme">
+	<Pane snapSize={15} maxSize={50}>
+        <GripVertical class="grip-vertical"/>
+        <Splitpanes horizontal theme="custom-theme">
+            <Pane minSize={20}>
+                <GripHorizontal class="grip-horizontal"/>
+                <div>Left</div>
+			</Pane>
+			<Pane minSize={20}>
+				<div>Right</div>
+			</Pane>
+		</Splitpanes>
+	</Pane>
+	<Pane></Pane>
+</Splitpanes>
+
+<style lang="scss">
+	@use '../../../lib/css/globals_forward.scss' as g;
+
+</style>
--- src/src/routes/+page.svelte
+++ src/src/routes/+page.svelte
@@ -1,27 +1,6 @@
 <script>
-	import { Pane, Splitpanes } from 'svelte-splitpanes';
+    import App from '$lib/components/window/App.svelte';
+    
 </script>
 
-<Splitpanes class="main-split-pane" theme="custom-theme">
-    <Pane snapSize={15} maxSize={50}>
-
-        <Splitpanes horizontal theme="custom-theme">
-            <Pane minSize={20}>
-                <div>Left</div>
-            </Pane>
-            <Pane minSize={20}>
-                <div>Right</div>
-            </Pane>
-        </Splitpanes>
-    </Pane>
-    <Pane>
-
-    </Pane>
-    
-</Splitpanes>
-
-<style global lang="scss">
-	@use "../lib/css/globals_forward.scss" as g;
-
-    
-</style>
+<App/>
