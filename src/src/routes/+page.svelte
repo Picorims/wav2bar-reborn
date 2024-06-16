d8e1e69c495ae5f924764b58f1b2f1cdedd82642
Picorims <picorims.contact@gmail.com>
2024-06-16 16:40:28 +0200

    ✨ feat(front): setup panes

--- src/src/routes/+page.svelte
+++ src/src/routes/+page.svelte
@@ -1,8 +1,27 @@
-<p>hi</p>
+<script>
+	import { Pane, Splitpanes } from 'svelte-splitpanes';
+</script>
+
+<Splitpanes class="main-split-pane" theme="custom-theme">
+    <Pane snapSize={15} maxSize={50}>
+
+        <Splitpanes horizontal theme="custom-theme">
+            <Pane minSize={20}>
+                <div>Left</div>
+            </Pane>
+            <Pane minSize={20}>
+                <div>Right</div>
+            </Pane>
+        </Splitpanes>
+    </Pane>
+    <Pane>
+
+    </Pane>
+    
+</Splitpanes>
 
 <style global lang="scss">
 	@use "../lib/css/globals_forward.scss" as g;
-    p {
-        @include g.theme-dark;
-    }
+
+    
 </style>
