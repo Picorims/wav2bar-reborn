28b3fe7ca53aefd2b628f25f0014410e133c8718
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-03-29 20:45:40 +0100

    ✨ feat(log): configure logger

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,21 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .plugin(tauri_plugin_log::Builder::new().build())
+    .plugin(tauri_plugin_log::Builder::new()
+      .level(log::LevelFilter::Trace)
+      .target(tauri_plugin_log::Target::new(
+        tauri_plugin_log::TargetKind::Stdout)
+      )
+      // .target(tauri_plugin_log::Target::new(
+      //   tauri_plugin_log::TargetKind::Webview)
+      // )
+      .target(tauri_plugin_log::Target::new(
+        tauri_plugin_log::TargetKind::LogDir {
+          file_name: Some("logs".to_string()),
+        },
+      ))
+      .build()
+    )
     .plugin(tauri_plugin_opener::init())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
