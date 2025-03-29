c9734445c64bb2dbe951a9efb92f608c78eb8dce
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-03-29 19:47:12 +0100

    🔧 build(tauri): add opener plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,7 @@
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_log::Builder::new().build())
+    .plugin(tauri_plugin_opener::init())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
