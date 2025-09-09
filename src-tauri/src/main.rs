05348fba403cda15adea1b9753b914936c0f10bf
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-09-09 23:41:32 +0200

    ✨ feat(log): try env_logger crate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env::{self, current_dir};
+
+use env_logger::Env;
+
 fn main() {
-  app_lib::run();
+    let env = Env::default()
+        .filter_or("MY_LOG_LEVEL", "trace");
+
+    env_logger::init_from_env(env);
+
+    log::info!("Initializing tauri...");
+
+    app_lib::run();
+
+    current_dir().ok().and_then(|path| {
+        log::info!("Current dir: {}\n", path.display());
+        Some(())
+    });
 }
