e72ac50ec4da1c84647567423aea7af5a7e30b6c
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-12-13 14:59:20 +0100

    📃 docs(particles): add note to reduce repetitive conversions for optimization

--- src/lib/engine/video/visual_objects/vo_particle_flow.ts
+++ src/lib/engine/video/visual_objects/vo_particle_flow.ts
@@ -152,6 +152,9 @@ export class VO_ParticleFlow implements VisualObjectRenderer<SaveVO_ParticleFlow
         } else if (this._flowType === "directional") {
             // to spread particles evenly, the amount spawned on each side depends on the flow direction
             // IMPORTANT: 90° is down because the y axis goes downwards in screen coordinates
+
+            // FIXME: Consider optimization by not redoing the conversion each time
+
             const verticalProbability = Math.abs(Math.sin(this._flowDirection * (Math.PI / 180)));
             // horizontal probability = 1 - verticalProbability; so deduced and not needed.
 
