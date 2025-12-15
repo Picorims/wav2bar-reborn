4b94fb69981bc67e0cbc434eb0c003657ef826e7
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-12-15 16:40:51 +0100

    🌈 style(project): format

--- src/lib/components/panes/controller_pane/FPSDisplay.svelte
+++ src/lib/components/panes/controller_pane/FPSDisplay.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { renderer } from "$lib/engine/video/renderer";
-	import { onMount } from "svelte";
+	import { renderer } from '$lib/engine/video/renderer';
+	import { onMount } from 'svelte';
 
 	/*
 	Wav2Bar - Free software for creating audio visualization (motion design) videos
@@ -11,23 +11,22 @@
 	file, You can obtain one at https://mozilla.org/MPL/2.0/.
 	*/
 
-    let rendererFps = $state(0);
-    let simulationTps = $state(0);
-    let interval = $state<ReturnType<typeof setInterval> | null>(null);
-    const REFRESH_INTERVAL_MS = 500;
+	let rendererFps = $state(0);
+	let simulationTps = $state(0);
+	let interval = $state<ReturnType<typeof setInterval> | null>(null);
+	const REFRESH_INTERVAL_MS = 500;
 
-    onMount(() => {
-        interval = setInterval(() => {
-            rendererFps = Math.round(renderer.getPerfFPS());
-            simulationTps = Math.round(renderer.getPerfTPS());
-        }, REFRESH_INTERVAL_MS);
-        return () => {
-            if (interval !== null) {
-                clearInterval(interval);
-            }
-        };
-    });
-        
+	onMount(() => {
+		interval = setInterval(() => {
+			rendererFps = Math.round(renderer.getPerfFPS());
+			simulationTps = Math.round(renderer.getPerfTPS());
+		}, REFRESH_INTERVAL_MS);
+		return () => {
+			if (interval !== null) {
+				clearInterval(interval);
+			}
+		};
+	});
 </script>
 
 <span class="fps">{rendererFps} FPS | {simulationTps} TPS</span>
@@ -35,8 +34,8 @@
 <style lang="scss">
 	@use '../../../css/globals_forward.scss' as g;
 
-    .fps {
-        @include g.text;
-        color: g.$color-text-800;
-    }
+	.fps {
+		@include g.text;
+		color: g.$color-text-800;
+	}
 </style>
