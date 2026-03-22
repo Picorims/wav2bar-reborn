13f9185e687dc4635ea44f0851d11eae655b59ff
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2026-03-23 00:24:09 +0100

    ✨ feat(visual object): make some defaults more convenient

--- src/lib/store/save.svelte.ts
+++ src/lib/store/save.svelte.ts
@@ -101,10 +101,27 @@ class SaveManager {
 					validateSaveVisualObject.errors?.map((e) => `- ${e.instancePath} ${e.message}`).join('\n')
 			);
 		}
+
+		
 		// type is generated with the schema defaults,
 		// and the TypeScript type originates from the same schema.
 		// But TypeScript is not able to infer that here.
-		return baseObject as unknown as VisualObject;
+		const typedObject = baseObject as unknown as VisualObject;
+
+		// overrides not doable through the schema:
+		if (typedObject.visual_object_type === "shape") {
+			typedObject.size = {
+				width: 400,
+				height: 300
+			}
+		} else if (typedObject.visual_object_type === "visualizer_circular_bar") {
+			typedObject.size = {
+				width: 400,
+				height: 400
+			}
+		}
+
+		return typedObject;
 	}
 
 	public async openSave(renderer: Renderer) {
