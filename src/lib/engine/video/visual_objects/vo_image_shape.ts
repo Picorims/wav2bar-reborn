b10116b0d5f43da5bbf96e610ec1c8d98f8955e8
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2026-03-28 12:14:24 +0100

    (image shape): add todo for potential memory leak

--- src/lib/engine/video/visual_objects/vo_image_shape.ts
+++ src/lib/engine/video/visual_objects/vo_image_shape.ts
@@ -208,6 +208,7 @@ export class VO_ImageShape implements VisualObjectRenderer<SaveVO_ImageShape> {
 			obj,
 			this.graphicsContext,
 			new MaskFilter({
+				// TODO fix potential GPU memory leak
 				sprite: new Sprite(renderer.generateTexture(globalMask)),
 			})
 		);
