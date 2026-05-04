9a2ebb8e70a02952db223b0181b1823ec6ac41f8
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2026-05-04 14:05:54 +0200

    🐳 chore(license): bump copyright year

--- src/lib/icons/LICENSE.md
+++ src/lib/icons/LICENSE.md
@@ -1,1 +1,1 @@
-Wav2Bar icons © 2025 by Picorims aka Charly Schmidt is licensed under Creative Commons Attribution 4.0 International. To view a copy of this license, visit https://creativecommons.org/licenses/by/4.0/
+Wav2Bar icons © 2025-2026 by Picorims aka Charly Schmidt is licensed under Creative Commons Attribution 4.0 International. To view a copy of this license, visit https://creativecommons.org/licenses/by/4.0/
--- src/lib/types/schemas/save_v4.d.ts
+++ src/lib/types/schemas/save_v4.d.ts
@@ -80,7 +80,7 @@ export type VisualizerCircularBar = {
  * Schema for Wav2Bar save files, version 4. (For versions beta 0.3.0 indev to beta 0.3.4).
  *
  *  Wav2Bar - Free software for creating audio visualization (motion design) videos.
- *  Copyright (C) 2025  Picorims <picorims.contact@gmail.com>
+ *  Copyright (C) 2025-2026  Picorims <picorims.contact@gmail.com>
  *
  *
  *  This program is free software: you can redistribute it and/or modify
